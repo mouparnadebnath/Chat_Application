a184301934e1737962ef70800310796750ef53b3
Mouparna Debnath <123438626+mouparnadebnath@users.noreply.github.com>
2024-06-01 10:35:10 +0530

    Update SocketContext.jsx

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -13,7 +13,7 @@ export const SocketContextProvider = ({ children }) => {
 console.log(authUser)
 	useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chat-application-kqbt.onrender.com", {
                 query: {
                     userId: authUser._id,
                 },
