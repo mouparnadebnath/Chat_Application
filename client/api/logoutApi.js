a020091216a71027fbb95e4ac019a9ba6dd55806
Mouparna Debnath <123438626+mouparnadebnath@users.noreply.github.com>
2024-06-01 13:34:46 +0530

    Update logoutApi.js

--- client/api/logoutApi.js
+++ client/api/logoutApi.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const url='http://localhost:5000/api/auth'
+const url='https://chat-application-kqbt.onrender.com/api/auth'
 export const userLogout=async(data)=>{
 try {
     const response=await axios.post(`${url}/logout`,data)
